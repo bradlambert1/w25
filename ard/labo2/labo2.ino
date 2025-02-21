f02c87b4aaee55f36ed754e6688c17328f7e0c03
merulox <bradlambert@live.ca>
2025-02-21 09:50:39 -0500

    things

--- ard/labo2/labo2.ino
+++ ard/labo2/labo2.ino
@@ -4,25 +4,50 @@ const int broche_button = 2;
 
 void setup() {
   Serial.begin(115200);
+  pinMode(broche_button, INPUT_PULLUP);
+
 }
  
-void potentium() {
+void potentium(int& pot) {
   const int interval = 1000;
   static unsigned long previousTime = currentTime;
   if(currentTime - previousTime >= interval){
     previousTime = currentTime;
     int potValue = analogRead(broche);
-    int mappedValue = map(potValue, 0, 1023, 0, 20);
-    Serial.println(mappedValue);
+    pot = map(potValue, 0, 1023, 0, 20);
   }
 }
-void button() {
-  
+void button(int pot) {
+  static int etatPrecedent = HIGH; 
+  static int state = HIGH;
+  const int interval = 25;
+  static unsigned long previousTime = currentTime;
+  static unsigned int currentState = digitalRead(broche_button);
+  if (currentState != previousState) {
+    previousTime = currentTime;
+  }
+  static unsigned int previousState = currentTime
+  if(currentTime - previousTime >= interval && currentState != state){
+    state = currentState;
+    previousTime = currentTime;
+    if (analogRead(2) == 1) {
+     Serial.print('[');
+      for (int i = 0; i < 20; i++) {
+        if (pot >= i) {
+          Serial.print('#');
+        } else {
+          Serial.print('.');
+        }
+      }
+      Serial.println(']');
+    }
+  }
 }
 void loop() {
   currentTime = millis();
+  int pot = 0;
 
-  potentium();
-  button();  
+  potentium(pot);
+  button(pot);  
   //Exécuter d'autres choses
 }
