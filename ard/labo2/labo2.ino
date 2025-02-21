d3b24784c9e5344f12cce94cb7f58212e9dbe15b
bradlambert1 <bradlambert@live.ca>
2025-02-21 14:44:57 -0500

    Update labo2.ino

--- ard/labo2/labo2.ino
+++ ard/labo2/labo2.ino
@@ -1,36 +1,53 @@
 unsigned long currentTime = 0;
 const int broche = A1;
-const int broche_button = 2;
+const int PIN_BUTTON = 2;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(broche_button, INPUT_PULLUP);
+  pinMode(PIN_BUTTON, INPUT_PULLUP);
 
 }
  
-void potentium(int& pot) {
+int estClic(unsigned long ct) {
+  static unsigned long lastTime = 0;
+  static int lastState = HIGH;
+  const int rate = 50;
+  int clic = 0;
+
+  if (ct - lastTime < rate) {
+    return clic; // Trop rapide
+  }
+
+  lastTime = ct;
+
+  int state = digitalRead(PIN_BUTTON);
+
+  if (state == LOW) {
+    if (lastState == HIGH) {
+      clic = 1;
+    }
+  }
+
+  lastState = state;
+
+  return clic;
+}
+
+int potentium() {
   const int interval = 1000;
   static unsigned long previousTime = currentTime;
+  int pot = 0;
   if(currentTime - previousTime >= interval){
     previousTime = currentTime;
     int potValue = analogRead(broche);
     pot = map(potValue, 0, 1023, 0, 20);
+    return pot;
   }
+ 
 }
 void button(int pot) {
-  static int previousState = HIGH; 
-  static int state = HIGH;
-  const int interval = 10;
-  static unsigned long previousTime = currentTime;
-  static unsigned int currentState = digitalRead(broche_button);
-  static unsigned long lastChange = 0;
-  if (currentState != previousState) {
-    lastChange = millis();
-  }
-  if(millis() - lastChange >= interval && currentState != state){
-    PreviousState = currentState;
-    previousTime = currentTime;
-    if (analogRead(2) == 1) {
+  if(estClic(currentTime)){
+    if (pot % 2 == 0) {
      Serial.print('[');
       for (int i = 0; i < 20; i++) {
         if (pot >= i) {
@@ -40,14 +57,55 @@ void button(int pot) {
         }
       }
       Serial.println(']');
+      Serial.print("  ");
+      Serial.print((pot*100)/20);
+      Serial.print("%");
+    } 
+    } else {
+      Serial.print((pot*100)/20);
+      Serial.print("%  ");
+      Serial.print('[');
+      for (int i = 0; i < 20; i++) {
+        if (pot >= i) {
+          Serial.print('#');
+        } else {
+          Serial.print('.');
+        }
+      }
+      Serial.println(']');
     }
+    
   }
-}
+
+
 void loop() {
   currentTime = millis();
-  int pot = 0;
-
-  potentium(pot);
+  int pot = potentium();
   button(pot);  
   //Exécuter d'autres choses
 }
+
+// static int previousState = HIGH; 
+//   static int state = HIGH;
+//   const int interval = 10;
+//   static unsigned long previousTime = currentTime;
+//   static unsigned int currentState = digitalRead(broche_button);
+//   static unsigned long lastChange = 0;
+//   if (currentState != previousState) {
+//     lastChange = millis();
+//   }
+//   if(millis() - lastChange >= interval && currentState != state){
+//     previousState = currentState;
+//     previousTime = currentTime;
+//     if (analogRead(2) == 1) {
+    //  Serial.print('[');
+    //   for (int i = 0; i < 20; i++) {
+    //     if (pot >= i) {
+    //       Serial.print('#');
+    //     } else {
+    //       Serial.print('.');
+    //     }
+    //   }
+    //   Serial.println(']');
+//     }
+//   }
