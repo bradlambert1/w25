1d340703736068a1066d815b3afd73235683d881
merulox <bradlambert@live.ca>
2025-03-14 12:35:49 -0400

    things

--- ard/labo3/labo3.ino
+++ ard/labo3/labo3.ino
@@ -1,5 +1,5 @@
 unsigned long currentTime = 0;
-const int broche = A1;
+const int photometer= A0;
 const int PIN_BUTTON = 2;
 
 void setup() {
@@ -10,12 +10,43 @@ void setup() {
 void name() {
   //this should last 3 seconds,
 }
+void printLuminosity(bool isOn) {
+  //LCD : Affiche la luminosité (ex. en %) et « Phares: ON/OFF ».
+}
 void photores() {
-  
+  float currentLuminosity = analogRead(photometer);
+  static unsigned long underTimer = 0;
+  static unsigned long overTimer = 0;
+
+  if (currentLuminosity < 0.5) {
+    underTimer += currentTime - lastTime;
+  } else {
+    underTimer = 0;
+  }
+  if (currentLuminosity > 0.5) {
+    overTimer += currentTime - lastTime;
+  } else {
+    overTimer = 0;
+  }
+
+
+  if (underTimer >= 5000) {
+    // turn on LEDS and print
+    printLuminosity(1);
+  }
+  if (overTimer >= 5000) {
+    //turn off LEDS and print
+
+    printLuminosity(0);
+  }
+
+
+  static unsigned long lastTime = currentTime;
+  static float lastLuminority = currentLuminosity;
 }
 
 void loop() {
+  if (currentTime == 0) ? name();
   currentTime = millis();
-  name();
   photores();
 }
