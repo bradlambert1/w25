7b2d6d43cf0a9d775753aae14ad21dfc53715aa7
Brad Lambert <bradlambert@live.ca>
2025-03-21 15:54:43 -0400

    things

--- ard/labo4/labo4/labo4.ino
+++ ard/labo4/labo4/labo4.ino
@@ -1,10 +1,32 @@
+// Distance
+#include <HCSR04.h>
+#define TRIGGER_PIN 9
+#define ECHO_PIN 10
+
+HCSR04 hc(TRIGGER_PIN, ECHO_PIN);
+
+// LCD
 #include <LiquidCrystal.h>
+
 const int rs = 36, en = 34, d4 = 32, d5 = 30, d6 = 28, d7 = 26;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+// State Machine
+enum State {STARTUP, WORKING};
+State state = STARTUP;
+
+// Global vars
+unsigned long currentTime = 0;
+string DA = "2471005"
+
 void setup() {
   // put your setup code here, to run once:
-  Serial.port(9600)
+  Serial.begin(9600);
+  // Distance
+  pinMode(TRIGGER_PIN, OUTPUT);
+  PinMode(ECHO_PIN, INPUT);
+  // LCD
+  lcd.begin(16, 2);
 }
 
 void printDistance(string da, int dist, int deg) {
@@ -16,28 +38,80 @@ void printDistance(string da, int dist, int deg) {
   Serial.println(deg);
 }
 
-StartupPrint() {
-  lcd.SetCursor(0, 0);
+
+void stateStartup() {
+  
+  lcd.clear();
   lcd.print("2471005");
   lcd.SetCursor(0,1);
   lcd.print("Labo 4B");
   delay(2000);
+  
+  lcd.clear();
+
+  state = WORKING;
 }
 
-void stateStartup() {
-  static bool firstRun = true;
-  if (firstRun) {
-    StartupPrint();
-    firstRun = false;
+void measureDistance() {
+  float distance = hc.dist();
+
+  return distance;
+}
+
+void lcdDisplay(float distance, int deg) {
+  
+  lcd.clear();
+  lcd.print("Dist  : ", distance);
+  lcd.SetCursor(0,1);
+  if (distance < 30) {
+    lcd.print("Obj  : Trop pret");
+  } else if (distance >= 30 && distance <=60) {
+    lcd.print("Obj  : ", deg, " deg");
+  } else {
+    lcd.print("Obj  : Trop loin");
   }
+  
 }
 
+void stateWorking() {
+  static unsigned long lastMeasureTime = 0;
+  static unsigned long lastPrintTime = 0;
+  const int MeasureRate = 50;
+  const int printRate = 100;
+  int deg = 0;
+  float distance = 0;
+
+
+  if (currentTime - lastMeasureTime >= MeasureRate) {
+    distance = measureDistance();
+    lastMeasureTime = currentTime;
+  
+    if (distance >= 30 && distance <= 60) {
+      deg = map(distance, 30, 60, 0, 180);
+    }
+    
+    lcdDisplay(distance, deg);
+
+    if (currentTime - lastPrintTime >= printRate) {
+      printDistance(DA, distance, deg);
+      lastMeasureTime = currentTime;
+    }
+  }
+  
+
+}
 
 
 void loop() {
   // put your main code here, to run repeatedly:  
-  stateStartup();
+  currentTime = millis();
 
-
-
+  switch (state) {
+    case STARTUP:
+      stateStartup();
+      break;
+    case WORKING:
+      stateWorking();
+      break;
+  }
 }
