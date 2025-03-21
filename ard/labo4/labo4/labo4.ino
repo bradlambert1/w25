896197d53da6fa2456b6ef9a783cfdacb267b366
Brad Lambert <bradlambert@live.ca>
2025-03-21 14:22:23 -0400

    things

--- ard/labo4/labo4/labo4.ino
+++ ard/labo4/labo4/labo4.ino
@@ -1,3 +1,7 @@
+#include <LiquidCrystal.h>
+const int rs = 36, en = 34, d4 = 32, d5 = 30, d6 = 28, d7 = 26;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 void setup() {
   // put your setup code here, to run once:
   Serial.port(9600)
@@ -12,9 +16,28 @@ void printDistance(string da, int dist, int deg) {
   Serial.println(deg);
 }
 
+StartupPrint() {
+  lcd.SetCursor(0, 0);
+  lcd.print("2471005");
+  lcd.SetCursor(0,1);
+  lcd.print("Labo 4B");
+  delay(2000);
+}
+
+void stateStartup() {
+  static bool firstRun = true;
+  if (firstRun) {
+    StartupPrint();
+    firstRun = false;
+  }
+}
+
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedly:  
+  stateStartup();
+
+
 
 }
